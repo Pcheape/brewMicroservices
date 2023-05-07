26ee2b4a36ce4a3a76b78c21aab67dcc43753d69
phillip.cheape@gmail.com <phillip.cheape@gmail.com>
2023-05-07 16:20:43 +0100

    initial commit

---
+++ src/main/java/com/phills/brewery/BreweryApplication.java
@@ -0,0 +1,13 @@
+package com.phills.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryApplication.class, args);
+	}
+
+}
